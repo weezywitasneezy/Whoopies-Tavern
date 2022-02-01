ec0f8a07f6868b3ef221915b28c39ec5a707fd57
weezywitasneezy <35600811+weezywitasneezy@users.noreply.github.com>
2022-01-31 18:33:17 -0700

    Add files via upload

---
+++ GazeableObject.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeableObject : MonoBehaviour
+{
+    public bool isTransformable = false;
+
+    private int objectLayer;
+    private const int IGNORE_RAYCAST_LAYER = 2;
+
+    private Vector3 initialObjectRotation;
+    private Vector3 initialPlayerRotation;
+    private Vector3 initialObjectScale;
+
+
+    public virtual void OnGazeEnter(RaycastHit hitInfo)
+    {
+        Debug.Log("Gaze entered on " + gameObject.name);
+    }
+
+    public virtual void OnGaze(RaycastHit hitInfo)
+    {
+        Debug.Log("Gaze hold on " + gameObject.name);
+
+    }
+
+    public virtual void OnGazeExit()
+    {
+        Debug.Log("Gaze exited on  " + gameObject.name);
+    }
+
+    public virtual void OnPress(RaycastHit hitInfo)
+    {
+        Debug.Log("Button pressed");
+
+        if (isTransformable)
+        {
+            objectLayer = gameObject.layer;
+            
+            gameObject.layer = IGNORE_RAYCAST_LAYER;
+
+            initialObjectRotation = transform.rotation.eulerAngles;
+            initialPlayerRotation = Camera.main.transform.eulerAngles;
+
+            initialObjectScale = transform.localScale;
+
+        }
+    }
+
+    public virtual void OnHold(RaycastHit hitInfo)
+    {
+        Debug.Log("Button hold");
+
+        if (isTransformable)
+        {
+            GazeTransform(hitInfo);
+        }
+    }
+
+    public virtual void OnRelease(RaycastHit hitInfo)
+    {
+        Debug.Log("Button released");
+
+        if (isTransformable)
+        {
+            gameObject.layer = objectLayer;
+        }
+    }
+
+    public virtual void GazeTransform(RaycastHit hitInfo)
+    {
+        // call the correct translation function
+        switch (Player.instance.activeMode)
+        {
+            // call different cases for activeMode
+            case InputMode.TRANSLATE:
+                GazeTranslate(hitInfo);
+                break;
+
+            case InputMode.ROTATE:
+                GazeRotate(hitInfo);
+                break;
+
+            case InputMode.SCALE:
+                GazeScale(hitInfo);
+                    break;
+        }
+    }
+
+    public virtual void GazeTranslate(RaycastHit hitInfo)
+    {
+        // move the object position
+
+        if (hitInfo.collider != null && hitInfo.collider.GetComponent<Floor>())
+        {
+            transform.position = hitInfo.point;
+        }
+    }
+
+    public virtual void GazeRotate(RaycastHit hitInfo)
+    {
+        // change the objects rotation
+
+        float rotationSpeed = 5.0f;
+
+        Vector3 currentPlayerRotation = Camera.main.transform.rotation.eulerAngles;
+
+        Vector3 currentObjectRotation = transform.rotation.eulerAngles;
+
+        Vector3 rotationDelta = currentPlayerRotation - initialPlayerRotation;
+
+        Vector3 newRotation = new Vector3(currentObjectRotation.x, initialObjectRotation.y + (rotationDelta.y * rotationSpeed), currentObjectRotation.z);
+
+        transform.rotation = Quaternion.Euler(newRotation);
+
+
+    }
+
+    public virtual void GazeScale(RaycastHit hitInfo)
+    {
+        // make the object bigger or smaller
+
+        float scaleSpeed = 0.1f;
+
+        float scaleFactor = 1;
+
+        Vector3 currentPlayerRotation = Camera.main.transform.rotation.eulerAngles;
+        Vector3 rotationDelta = currentPlayerRotation - initialPlayerRotation;
+
+        // if looking  up
+        if (rotationDelta.x < 0 && rotationDelta.x > -180.0f || rotationDelta.x > 180.0f && rotationDelta.x < 360.0f)
+        {
+
+            //if greater than 180, map it between 0 - 180
+            if (rotationDelta.x > 180.0f)
+            {
+                rotationDelta.x = 360.0f - rotationDelta.x;
+            }
+
+
+            scaleFactor = 1.0f + Mathf.Abs(rotationDelta.x) * scaleSpeed;
+
+        }
+        else
+        {
+
+            if (rotationDelta.x < -180.0f)
+            {
+                rotationDelta.x = 360.0f + rotationDelta.x;
+            }
+
+            scaleFactor = Mathf.Max(0.1f, 1.0f - (Mathf.Abs(rotationDelta.x) * (1.0f/scaleSpeed)) / 180.0f);
+
+        }
+
+        transform.localScale = scaleFactor * initialObjectScale;
+
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputMode
+{
+    NONE,
+    TELEPORT,
+    WALK,
+    FURNITURE,
+    TRANSLATE,
+    ROTATE,
+    SCALE
+
+}
+
+public class Player : MonoBehaviour
+{
+
+    public static Player instance;
+
+    public InputMode activeMode = InputMode.NONE;
+
+    public Object activeFurniturePrefab;
+
+    [SerializeField]
+    private float playerSpeed = 3.0f;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(instance.gameObject);
+        }
+
+        instance = this;
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryWalk();
+    }
+    public void TryWalk()
+    {
+        if (Input.GetMouseButton(0) && activeMode == InputMode.WALK)
+        {
+            Vector3 forward = Camera.main.transform.forward;
+
+            Vector3 newPosition = transform.position + forward * Time.deltaTime * playerSpeed;
+
+            transform.position = newPosition;
+        }
+    }
+
+}
